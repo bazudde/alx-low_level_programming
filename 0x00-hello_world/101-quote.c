04aa3ec93378f48aa3973431b9162abaa19fe7a5
Theresa Bazudde <bazuddetheresa@gmail.com>
2022-03-10 14:43:21 -0800

    hi

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,11 +6,8 @@
 * Return: 1
 */
 int main(void)
-{
-		int s = strlen("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
-
-			write(2,
-						      "and that piece of art is useful - Dora Korpar, 2015-10-19\n",
-						      	      s);
-				return (1);
+{int s = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+write(2,
+"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",s);
+return (1);
 }

72db79c8a9509d8e42822dd83a3a4c5a2ba63c2a
Theresa Bazudde <bazuddetheresa@gmail.com>
2022-03-15 12:32:51 -0700

    one

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,6 +6,10 @@
  */
 int main(void)
 {
-_putchar("%s\n", _putchar);
+ar word[8] = "_putchar";
+int i;
+for(i = 0; i < 8;i++)
+	_putchar(word[i]);
+_putchar('\n');
 		return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,1 @@
-int _putchar(const char *format, ...);
+int _putchar(char c);

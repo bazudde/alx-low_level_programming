7d83156f6440d3783658a149eba87ad0b73c202e
Theresa Bazudde <bazuddetheresa@gmail.com>
2022-03-15 14:36:28 -0700

    first function

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,20 @@
 #include "main.h"
 /**
- * main-print _putchar followed by a new line
- * return -0
+ * main - Print "_printchar" followed by a new line.
+ * Description: You are not allowed to include standard libraries.
+ * Return: 0
  */
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-	return(0);
+	char holberton[] = "_putchar";
+	int i = 0;
+
+	while (holberton[i] != '\0')
+	{
+		_putchar(holberton[i]);
+		i++;
+	}
+	_putchar('\n');
+
+	return (0);
 }

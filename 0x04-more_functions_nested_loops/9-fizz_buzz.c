dc078d94d1141eca5375d44298dc952de81b5c40
Theresa Bazudde <bazuddetheresa@gmail.com>
2022-03-17 11:10:03 -0700

    five

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,17 +13,21 @@ int main(void)
 	while (i <= 100)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
+		{
 			printf("FizzBuzz");
+		}
 		else if (i % 3 == 0)
+		{
 			printf("Fizz");
+		}
 		else if (i % 5 == 0)
+		{
 			printf("Buzz");
+		}
 		else
 			printf("%d", i);
 
-		if (i != 100)
-			printf(" ");
-		i++;
+			i++;
 	}
 	printf("\n");
 

1b216093042a3857515803ee9f5b6399b59d8e5b
Theresa Bazudde <bazuddetheresa@gmail.com>
2022-03-22 12:01:48 -0700

    every other chr

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -3,6 +3,7 @@
  * puts2 - prints every other character starting with the first one
  * @str: string to be printed
  */
+
 void puts2(char *str)
 {
 	int i;
@@ -10,7 +11,7 @@ void puts2(char *str)
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (i % 2 == 0)
-			 _putchar(str[i]);
+			_putchar(str[i]);
 	}
 	_putchar('\n');
 }

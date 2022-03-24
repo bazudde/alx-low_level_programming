2200bba84105cbc731fafd0a67adbb7eb836db21
Theresa Bazudde <bazuddetheresa@gmail.com>
2022-03-24 12:49:26 -0700

    no null terminate

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,21 +1,22 @@
 #include "main.h"
 /**
- * _strncat - concatenates two strings but src doesnot have to be null
- * terminated
- * @dest: char type pointer
- * @src:char type pointer 
- * return : dest
+ * _strncat - concatenate two strings
+ * @dest: string
+ * @src: string
+ * @n: number of elements to concatenate in
+ * Return: pointer to resulting `dest`
  */
+
 char *_strncat(char *dest, char *src, int n)
 {
-	int i, j;
-	
+	int i, c;
+
 	for (i = 0; dest[i] != '\0'; i++)
 		;
 
-	for (j = 0; src[j] != '\0' && n > 0; j++, n--, i++)
+	for (c = 0; src[c] != '\0' && n > 0; c++, n--, i++)
 	{
-		dest[i] = src[j];
+		dest[i] = src[c];
 	}
 	return (dest);
 }

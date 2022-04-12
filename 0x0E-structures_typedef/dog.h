a1462a49d50f63e1162c0b3aa35c68b97e4ead31
Theresa Bazudde <bazuddetheresa@gmail.com>
2022-04-12 16:04:17 -0700

    dog structure

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - structure definition f a dog
+ * @name: character pointer for dog name
+ * @age: float for dog age
+ * @owner: character of dog owner
+ */
+
+typedef struct dog
+{
+	float age;
+	char *name;
+	char *owner;
+}
+#endif
